e69400a2bd3fa37733cde86da6c5af0371f686ba
Farmata Kane <kane.farmata@gmail.com>
2024-09-23 10:02:30 +0100

    complete until: analyzeArray challange

---
+++ __test__/5-analyzeArray.test.js
@@ -0,0 +1,86 @@
+const analyzeArray = require("../katas/5-analyzeArray")
+
+describe("analyzeArray()", () => {
+  test("should return an object", () => {
+    expect(analyzeArray()).toBeObject();
+  });
+  test("should have a method of length that returns a number", () => {
+    expect(analyzeArray()).toContainKey(["length"]);
+    expect(analyzeArray().length).toBeNumber();
+  });
+  test("should have a method of min that returns a number", () => {
+    expect(analyzeArray()).toContainKey(["min"]);
+    expect(analyzeArray().min).toBeNumber();
+  });
+  test("should have a method of max that returns a number", () => {
+    expect(analyzeArray()).toContainKey(["max"]);
+    expect(analyzeArray().max).toBeNumber();
+  });
+  test("should have a method of average that returns a number", () => {
+    expect(analyzeArray()).toContainKey(["average"]);
+    expect(analyzeArray().average).toBeNumber();
+  })
+  // ---> length
+  describe("length", () => {
+    test("if passed with an empty array it should return 0", () => {
+      const testArr = analyzeArray([]);
+      expect(testArr.length).toBe(0);
+    });
+    test("if passed with an array containing a single value it should return 1", () => {
+      const testArr = analyzeArray([1]);
+      expect(testArr.length).toBe(1);
+    });
+    test("if passed with an array with multiple values, it should return the length of the array", () => {
+      const testArr = analyzeArray([1, 2, 3]);
+      expect(testArr.length).toBe(3);
+    });
+  });
+
+  // ---> min 
+  describe("min", () => {
+    test("if passed with an empty array, it should return 0", () => {
+      const testArr = analyzeArray([]);
+      expect(testArr.min).toBe(0);
+    });
+    test("if passed with an array containing a single value it should return that value", () => {
+      const testArr = analyzeArray([5]);
+      expect(testArr.min).toBe(5);
+    });
+    test("if passed with an array with multiple values, it should return the smallest value of the array", () => {
+      const testArr = analyzeArray([1, 2, 3]);
+      expect(testArr.min).toBe(1);
+    });
+  });
+
+  // ---> max 
+  describe("max", () => {
+    test("if passed with an empty array, it should return 0", () => {
+      const testArr = analyzeArray([]);
+      expect(testArr.max).toBe(0);
+    });
+    test("if passed with an array containing a single value it should return that value", () => {
+      const testArr = analyzeArray([5]);
+      expect(testArr.max).toBe(5);
+    });
+    test("if passed with an array with multiple values, it should return the smallest value of the array", () => {
+      const testArr = analyzeArray([1, 2, 3]);
+      expect(testArr.max).toBe(3);
+    });
+  });
+
+  // ---> average 
+  describe("average", () => {
+    test("if passed with an empty array, it should return 0", () => {
+      const testArr = analyzeArray([]);
+      expect(testArr.average).toBe(0);
+    });
+    test("if passed with an array containing a single value it should return that value", () => {
+      const testArr = analyzeArray([5]);
+      expect(testArr.average).toBe(5);
+    });
+    test("if passed with an array with multiple values, it should return the average value of the array", () => {
+      const testArr = analyzeArray([10, 20, 30, 40]);
+      expect(testArr.average).toBe(25)
+    })
+  })
+})
---
+++ katas/5-analyzeArray.js
@@ -0,0 +1,64 @@
+/*An analyzeArray function that takes an array of numbers and returns an object with the following properties: average, min, max, and length.
+
+const object = analyzeArray([1,8,3,4,2,6]);
+
+object == {
+   average: 4,
+   min: 1,
+   max: 8,
+   length: 6
+};*/
+
+function analyzeArray(arr) {
+const objectAnalyze = {}
+
+objectAnalyze.average = getAverage(arr)
+objectAnalyze.min = getMin(arr);
+objectAnalyze.max = getMax(arr)
+objectAnalyze.length = getLength(arr);
+
+
+return objectAnalyze
+};
+
+function getLength(arr) {
+  if(!arr) return 0
+  else {
+    return arr.length
+  }
+};
+
+function getMin(arr) {
+  if(!arr || arr.length === 0) return 0
+
+  if(arr.length <= 1) { 
+    return arr[0];
+    } else { 
+      return Math.min(...arr) }
+};
+
+function getMax(arr) {
+  if(!arr || arr.length === 0) return 0
+
+  if(arr.length <= 1) { 
+    return arr[0];
+    } else { 
+      return Math.max(...arr) }
+};
+
+function getAverage(arr) {
+  if(!arr || arr.length === 0) return 0
+
+  if(arr.length <= 1) { 
+    return arr[0];
+  } else {
+    let sum = arr.reduce((acc, curr) => acc + curr, 0);
+    let average = sum / arr.length;
+    return average
+  }
+}
+
+
+console.log(analyzeArray([10, 20, 30, 40]))
+
+module.exports = analyzeArray
